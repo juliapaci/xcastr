b5735c3631ac7f244c0f9d7b2247d655ba8e351c
aymey <fabianpaci@gmail.com>
2022-11-17 20:05:44 +1100

    Interactable window and background

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 # Compiler flags
-CFLAGS = -lX11 -g -Wall
+CFLAGS = -lX11 -lm -lXfixes -g -Wall
 
 # Build Target
 TARGET = XCastr
--- XCastr.c
+++ XCastr.c
@@ -2,24 +2,35 @@
 #include <X11/Xatom.h>
 #include <X11/Xutil.h>
 
+#include <X11/extensions/Xfixes.h>
+#include <X11/extensions/shape.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 
 #include <unistd.h>
 
+
 #define BASIC_EVENT_MASK (StructureNotifyMask|ExposureMask|PropertyChangeMask|EnterWindowMask|LeaveWindowMask|KeyPressMask|KeyReleaseMask|KeymapStateMask)
 #define NOT_PROPAGATE_MASK (KeyPressMask|KeyReleaseMask|ButtonPressMask|ButtonReleaseMask|PointerMotionMask|ButtonMotionMask)
 
+typedef enum { false, true } bool;
+
+void WindowRemove();
+void WindowInteractable();
+
 int main(int argc, char *argv[]) {
+
+    // user variables
+    bool interactable = false;
+    int background = 0x00FF000000;
+
     Display *display = XOpenDisplay(NULL);
-    // int visual = DefaultVisual(display, screen);
     Window root = XDefaultRootWindow(display), window;
 
     XVisualInfo vinfo;
     XMatchVisualInfo(display, DefaultScreen(display), 32, TrueColor, &vinfo);
     Colormap colourmap = XCreateColormap(display, root, vinfo.visual, AllocNone);
-    // XColor backgroundColour = createXColorFromRGBA(0, 0, 0, 0);
-
 
     XWindowAttributes rootAttributes, windowAttributes;
 
@@ -33,22 +44,24 @@ int main(int argc, char *argv[]) {
     XSetWindowAttributes setWindowAttributes;
     setWindowAttributes.override_redirect = 1;
     setWindowAttributes.background_pixmap = None;
-    setWindowAttributes.background_pixel = "0x00FF000000";
-    setWindowAttributes.border_pixel=0;
-    setWindowAttributes.win_gravity=NorthWestGravity;
-    setWindowAttributes.bit_gravity=ForgetGravity;
-    setWindowAttributes.save_under=1;
-    setWindowAttributes.event_mask=BASIC_EVENT_MASK;
-    setWindowAttributes.do_not_propagate_mask=NOT_PROPAGATE_MASK;
+    setWindowAttributes.background_pixel = background;
+    setWindowAttributes.border_pixel = 0;
+    setWindowAttributes.win_gravity = NorthWestGravity;
+    setWindowAttributes.bit_gravity = ForgetGravity;
+    setWindowAttributes.save_under = 1;
+    setWindowAttributes.event_mask = BASIC_EVENT_MASK;
+    setWindowAttributes.do_not_propagate_mask = NOT_PROPAGATE_MASK;
     setWindowAttributes.colormap = colourmap;
 
 
-    unsigned long m = CWColormap | CWBorderPixel | CWBackPixel | CWEventMask | CWWinGravity|CWBitGravity | CWSaveUnder | CWDontPropagate | CWOverrideRedirect;
+    unsigned long mask = CWColormap | CWBorderPixel | CWBackPixel | CWEventMask | CWWinGravity | CWBitGravity | CWSaveUnder | CWDontPropagate | CWOverrideRedirect;
 
-    window = XCreateWindow(display, root, rootAttributes.x, rootAttributes.y, 200, 200, 0, vinfo.depth, InputOutput, vinfo.visual, m, &setWindowAttributes);
+    window = XCreateWindow(display, root, rootAttributes.x, rootAttributes.y, 200, 200, 0, vinfo.depth, InputOutput, vinfo.visual, mask, &setWindowAttributes);
 
-    // XGetWindowAttributes(display, window, &windowAttributes); // get window attributes
+    XGetWindowAttributes(display, window, &windowAttributes); // get window attributes
 
+    if(!interactable)
+        WindowInteractable(display, window);
 
 
 
@@ -60,13 +73,12 @@ int main(int argc, char *argv[]) {
 
 
     // Set window position to bottom right of screen
-    // XTranslateCoordinates(display, window, root, 0, 0, (unsigned int *) rootAttributes.width, (unsigned int *) rootAttributes.height, window);
+    // XTranslateCoordinates(display, window, root, 0, 0, (int *) rootAttributes.width, (unsigned int *) rootAttributes.height, window);
 
 
 
 
 
-    // TODO: set window floating
     property[1] = XInternAtom(display, "_NET_WM_WINDOW_TYPE", 0);
     property[0] = XInternAtom(display, "_NET_WM_WINDOW_TYPE_UTILITY", 0);
     XChangeProperty(display, window, property[0], XA_ATOM, 32, PropModeReplace, (unsigned char*) property, 2L);
@@ -86,7 +98,6 @@ int main(int argc, char *argv[]) {
 
 
     // TODO: render text with custom font
-    // TODO: set window backgroudn colour
     // TODO: remove decorations
     // TODO: Check if focus window asks for password and if it does then replace the characters with "*"
     // TODO: dynamically change window size by text
@@ -101,9 +112,20 @@ int main(int argc, char *argv[]) {
 
     sleep(10);
 
+    WindowRemove(display, window);
+
+    return 0;
+}
+
+void WindowRemove(Display *display, Window window) {
     XUnmapWindow(display, window);
     XDestroyWindow(display, window);
     XCloseDisplay(display);
-    return 0;
+}
+
+void WindowInteractable(Display *display, Window window) {
+    XserverRegion region = XFixesCreateRegion(display, NULL, 0);
+    XFixesSetWindowShapeRegion (display, window, ShapeInput, 0, 0, region);
+    XFixesDestroyRegion (display, region);
 }
 
